a7c82fe228cf6864385619e2adfbb36a3e4c8f73
LukasBartipan <lukasbartipan@gmail.com>
2020-08-15 00:05:12 +0200

    Added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Body from './components/body/Body';
+
+function App() {
+  return (
+    <div className="App">
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    
+    // logic
+
+    render () {
+        return (
+            <div className='header'>
+                <h1>SpotifyAPP</h1>    
+            </div>
+        )
+    }
+}
